efa368813c728dbfec0d0452c116c3c179c310f5
Avrour7 <105707196+Avrour7@users.noreply.github.com>
2025-09-03 14:35:28 +0300

    Add files via upload

---
+++ #1.py
@@ -0,0 +1,48 @@
+#1.1
+print (31, 18, 79)
+
+#1.2
+print (47, 52, 150, sep= '  ')
+
+#1.3
+print(50)
+print(10)
+
+#1.4
+print(5)
+print(10)
+print(21)
+
+#1.5
+print(1)
+print(2)
+
+#1.6
+import math
+print(f"{math.pi:.3f}")
+
+#1.7
+import math
+print(f"{math.e:.1f}")
+
+#1.8
+num = input("Введите число: ")
+print("Вы ввели число", num)
+
+#1.9
+num = input("Число: ")
+print(num, "-вот какое число Вы ввели")
+
+#1.10
+name = input("Введите имя: ")
+print(name)
+
+#1.11
+name = input("Введите название ФК: ")
+print(name, "-это чемпион!")
+
+#1.12
+name = input("Введите имя: ")
+print('Привет, ' + name + "!" )
+
+#1.13
